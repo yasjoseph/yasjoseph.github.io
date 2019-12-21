5b7fe9ad420bd25a125a0595ed29e4635faeeeb6
yasjoseph <youssufali334@gmail.com>
2019-12-21 12:54:28 +0530

    initialrepo

---
+++ precache-manifest.e5b8a89af0b12c2fe891565c46c4bb2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ad778de4cd47613edc075345e9e417",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b71e7542273335fa6356",
+    "url": "/static/css/main.98dc09af.chunk.css"
+  },
+  {
+    "revision": "cfcce7991f95df950996",
+    "url": "/static/js/2.942644b2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.942644b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b71e7542273335fa6356",
+    "url": "/static/js/main.fabca295.chunk.js"
+  },
+  {
+    "revision": "6682ad4c114d9301fed3",
+    "url": "/static/js/runtime-main.d07ec2c6.js"
+  },
+  {
+    "revision": "b98eb3ee0837584f8db8251d840a3e51",
+    "url": "/static/media/img2.b98eb3ee.jpg"
+  }
+]);
